6020430ac9ed5e1bb4b26192c1519bc285681529
Michael Beemer <michael.beemer@dynatrace.com>
2017-06-02 16:53:13 -0400

    Added timing information

--- core/index.js
+++ core/index.js
@@ -15,6 +15,7 @@ const DEFAULT_MESSAGE = "Oh no!  It looks like you don't have access.";
 
 const lockWolf = {
     authenticate: async (userId, deviceId) => {
+        console.time('authenticate');
         logger.info("Attempting to authenticate user");
         if (!userId) {
             throw new LWError("A user ID is required!", 400);
@@ -25,10 +26,12 @@ const lockWolf = {
         let authenticated = false;
         let message = DEFAULT_MESSAGE;
         let user, device
+        console.time('getUserAndDevice');
         [ user, device ] = await BbPromise.all([
             UserModel.get(userId),
             DeviceModel.get(deviceId)
         ]);
+        console.timeEnd('getUserAndDevice');
 
         if (!user) {
             throw new LWError("It looks like we haven't met.  Please contact the lab administrator for access!");
@@ -37,13 +40,19 @@ const lockWolf = {
         }
 
         // Checks if user has passed the cert
+        console.time('isCertified');
         const access = !await accessController.isCertified(user, device);
+        console.timeEnd('isCertified');
+
         if (!access) {
             throw new Error(`Hmm, it looks like you haven't passed the required certification in order to use a ${deviceUtils.getFriendlyCategory(device)}.`);
         }
 
         // Checks if the user is scheduled to use the device
+        console.time('isAvailable');
         const availability = await accessController.getAvailability(user, device);
+        console.timeEnd('isAvailable');
+
         if (!availability.available) {
             throw new Error(`Sorry, someone has already reserved the ${deviceUtils.getFriendlyCategory(device)}.`);
         }
@@ -53,7 +62,8 @@ const lockWolf = {
         message = (availability.minutesAllocated) ?
             `You're all set for the next ${availability.minutesAllocated} minutes, ${user.firstName}!`
             : `You're all set ${user.firstName}!`;
-
+        
+        console.timeEnd('authenticate');
         return {
             authenticated,
             message,
